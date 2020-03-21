2f2644ea54a8db84ca119881930473dca38e662a
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2020-03-21 19:19:09 +0300

    feat(pull custom git): -

---
+++ Functions/pull-custom.py
@@ -0,0 +1,11 @@
+
+import os
+path = "C:\\CODE_SEGMANT\\"
+for folder in os.listdir(path):
+    folder_path = os.path.join(path, folder)
+    print("doing for")
+    print(folder_path)
+    os.system("git -C {} pull --recurse-submodules".format(folder_path))
+    os.system("git -C {} -c diff.mnemonicprefix=false -c core.quotepath=false submodule update --init --recursive --remote --merge".format(folder_path))
+
+
